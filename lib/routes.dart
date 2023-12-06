1cc5e27fedbe704883c98c7314ff98083e7c0bf7
OMAR <s11925044@stu.najah.edu>
2023-12-06 12:41:12 +0200

    Add SignUp file Path to routes files

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/constant/routes.dart';
 import 'package:flutter_application_1/view/screen/auth/login.dart';
+import 'package:flutter_application_1/view/screen/auth/signup.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   AppRoute.login: (context) => const Login(),
+  AppRoute.signUp: (context) => const SignUp(),
 };
