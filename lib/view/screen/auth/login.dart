535ca339476c2ff16f61284691a9e3e970070020
OMAR <s11925044@stu.najah.edu>
2023-12-06 11:06:18 +0200

    Add hint text and icon for TextFormField

--- lib/view/screen/auth/login.dart
+++ lib/view/screen/auth/login.dart
@@ -11,56 +11,64 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     String space = "\t\t\t\t\t\t\t\t\t\t";
     return Scaffold(
-        //appBar: AppBar(title: Text("Contact")),
+        appBar: AppBar(
+          title: const Text("Sign In"),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
         body: ListView(
-      children: [
-        Form(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            //logo_image
-            const CustomLogoAuth(),
+            Form(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo_image
+                const CustomLogoAuth(),
 
-            //registressionNumber_field
-            const CustomTextFormAuth(
-              labeltext: "Regestression Number*",
-              obscureText: false,
-              textInputType: TextInputType.number,
-            ),
+                //registressionNumber_field
+                const CustomTextFormAuth(
+                  labeltext: "Regestression Number*",
+                  obscureText: false,
+                  textInputType: TextInputType.number,
+                  hinttext: 'Enter Your Number',
+                  iconData: Icons.numbers,
+                ),
 
-            //password_field
-            const CustomTextFormAuth(
-              labeltext: "Password*",
-              obscureText: true,
-              textInputType: TextInputType.text,
-            ),
+                //password_field
+                const CustomTextFormAuth(
+                  labeltext: "Password*",
+                  obscureText: true,
+                  textInputType: TextInputType.text,
+                  hinttext: 'Enter Your Password',
+                  iconData: Icons.lock_outline,
+                ),
 
-            //forgetPassword_button
-            Padding(
-                padding: const EdgeInsets.only(right: 20, bottom: 30),
-                child: CustomTextButtonAuth(
-                  textOfTextButton: "Forget Password ?",
-                  alignment: Alignment.centerRight,
+                //forgetPassword_button
+                Padding(
+                    padding: const EdgeInsets.only(right: 20, bottom: 30),
+                    child: CustomTextButtonAuth(
+                      textOfTextButton: "Forget Password ?",
+                      alignment: Alignment.centerRight,
+                      onPressed: () {},
+                    )),
+
+                //signin_button
+                CustomMaterialButtonAuth(
+                  text: "SIGN IN",
                   onPressed: () {},
-                )),
+                ),
 
-            //signin_button
-            CustomMaterialButtonAuth(
-              text: "SIGN IN",
-              onPressed: () {},
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 60),
-              child: CustomTextButtonAuth(
-                textOfTextButton: "Don't have account ?\n$space Sign Up",
-                onPressed: () {},
-                alignment: Alignment.center,
-              ),
-            )
+                Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: CustomTextButtonAuth(
+                    textOfTextButton: "Don't have account ?\n$space Sign Up",
+                    onPressed: () {},
+                    alignment: Alignment.center,
+                  ),
+                )
+              ],
+            )),
           ],
-        )),
-      ],
-    ));
+        ));
   }
 }
--- lib/view/widget/auth/customtextformauth.dart
+++ lib/view/widget/auth/customtextformauth.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CustomTextFormAuth extends StatelessWidget {
   final String labeltext;
+  final String hinttext;
+  final IconData iconData;
   final bool obscureText;
   final TextInputType textInputType;
   const CustomTextFormAuth(
       {super.key,
       required this.labeltext,
       required this.obscureText,
-      required this.textInputType});
+      required this.textInputType,
+      required this.hinttext,
+      required this.iconData});
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +23,13 @@ class CustomTextFormAuth extends StatelessWidget {
         obscureText: obscureText,
         keyboardType: textInputType,
         decoration: InputDecoration(
+          hintText: hinttext,
+          hintStyle: const TextStyle(fontSize: 12),
           contentPadding:
               const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           labelText: labeltext,
+          suffixIcon: Icon(iconData),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(100)),
         ),
       ),
