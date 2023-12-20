6feffc8fa83f8fba728e8ebb56f7abeffaa50f87
OMAR <s11925044@stu.najah.edu>
2023-12-20 10:32:44 +0200

    Build SuccessResetPassword Page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/constant/routes.dart';
-import 'package:flutter_application_1/view/screen/auth/forgetpassword.dart';
+import 'package:flutter_application_1/view/screen/auth/forgetpassword/forgetpassword.dart';
 import 'package:flutter_application_1/view/screen/auth/login.dart';
-import 'package:flutter_application_1/view/screen/auth/resetpassword.dart';
+import 'package:flutter_application_1/view/screen/auth/forgetpassword/resetpassword.dart';
 import 'package:flutter_application_1/view/screen/auth/signup.dart';
-import 'package:flutter_application_1/view/screen/auth/success_resetpassword.dart';
-import 'package:flutter_application_1/view/screen/auth/verifycode.dart';
+import 'package:flutter_application_1/view/screen/auth/forgetpassword/success_resetpassword.dart';
+import 'package:flutter_application_1/view/screen/auth/forgetpassword/verifycode.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   AppRoute.login: (context) => const Login(),
--- lib/view/screen/auth/forgetpassword/success_resetpassword.dart
+++ lib/view/screen/auth/forgetpassword/success_resetpassword.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/view/widget/auth/custommaterialbuttonauth.dart';
 
 class SuccessResetPassword extends StatelessWidget {
   const SuccessResetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Success"),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: const Column(
+          children: [
+            Center(
+              child: Icon(
+                Icons.check_circle_sharp,
+                size: 200,
+                color: Colors.green,
+              ),
+            ),
+            Text("Registration complete!"),
+            Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: CustomMaterialButtonAuth(text: "Start"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
