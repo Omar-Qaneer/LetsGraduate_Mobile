72aeae17a4b3ab915e06d080c813bff0ac3c2b9f
OMAR <s11925044@stu.najah.edu>
2023-12-25 18:34:51 +0200

    Edit MaterialButton
    Use Custom MaterialButton to minimize lines of code

--- lib/view/screen/auth/forgetpassword/resetpassword.dart
+++ lib/view/screen/auth/forgetpassword/resetpassword.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller/auth/resetpassword_controller.dart';
-import 'package:flutter_application_1/core/constant/imageasset.dart';
 import 'package:flutter_application_1/view/widget/auth/customlogoauth.dart';
+import 'package:flutter_application_1/view/widget/auth/custommaterialbuttonauth.dart';
 import 'package:flutter_application_1/view/widget/auth/customtextformauth.dart';
 import 'package:get/get.dart';
 
@@ -59,18 +59,12 @@ class ResetPassword extends StatelessWidget {
               ),
 
               //submitting_button
-              MaterialButton(
-                minWidth: 350,
+              CustomMaterialButtonAuth(
+                text: "Submitting...",
                 onPressed: () {
                   controller.goToSuccessResetPassword();
                 },
-                color: Colors.blue[800],
-                textColor: Colors.white,
-                child: const Text(
-                  "Submitting...",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
+              )
             ],
           )),
         ],
