79769de42f18e01d007bd74e7d67c788d52a8ec0
OMAR <s11925044@stu.najah.edu>
2023-12-26 11:21:33 +0200

    Edit MyGroups file 2
    Add ListTile Widget inside ListView.separated Widget to minize lines of code

--- lib/view/screen/supervisor/mygroups.dart
+++ lib/view/screen/supervisor/mygroups.dart
@@ -6,6 +6,62 @@ class SupervisorGroups extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final groups = [
+      {
+        {
+          "name": 'Omar Qaneer',
+          "number": 11925044,
+          "department": 'Computer Engineering',
+          "email": 'omarqaner@gmail.co',
+          "mobileNumber": 05954698745,
+          "address": 'Nablus',
+        },
+        {
+          "name": 'Jamal SaadEddin',
+          "number": 12345678,
+          "department": 'Computer Engineering',
+          "email": 'jamal@gmail.co',
+          "mobileNumber": 05954698745,
+          "address": 'Nablus',
+        }
+      },
+      {
+        {
+          "name": 'Obaida Aws',
+          "number": 119236866,
+          "department": 'Computer Engineering',
+          "email": 'obaidaaws@gmail.co',
+          "mobileNumber": 05954698745,
+          "address": 'Jenin',
+        },
+        {
+          "name": 'Abdallah Ads',
+          "number": 11945177,
+          "department": 'Computer Engineering',
+          "email": 'abdallahads@gmail.co',
+          "mobileNumber": 05954698745,
+          "address": 'Jenin',
+        },
+      },
+      {
+        {
+          "name": 'Omar Quzmar',
+          "number": 11924055,
+          "department": 'Computer Engineering',
+          "email": 'omarquzmar@gmail.co',
+          "mobileNumber": 05954698745,
+          "address": 'Jenin',
+        },
+        {
+          "name": 'Yahia Arafat',
+          "number": 11825047,
+          "department": 'Computer Engineering',
+          "email": 'yahiaarafat@gmail.co',
+          "mobileNumber": 05954698745,
+          "address": 'Jenin',
+        }
+      }
+    ];
     return ListView(
       children: [
         Container(
@@ -16,6 +72,43 @@ class SupervisorGroups extends StatelessWidget {
             horizontalPadding: 15,
             verticalPadding: 5,
           ),
+        ),
+        ListView.separated(
+          shrinkWrap: true,
+          itemCount: groups.length,
+          itemBuilder: (context, index) {
+            final group = groups[index];
+            final String groupLength = group.length.toString();
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundColor: const Color(0xff764abc),
+                child: Text(
+                  group.elementAt(0)['name'].toString().substring(0, 1),
+                  style: const TextStyle(fontSize: 30, color: Colors.white),
+                ),
+              ),
+              title: Text(
+                "Group $index",
+                style: const TextStyle(fontSize: 16),
+              ),
+              subtitle: Row(
+                children: [
+                  const Icon(Icons.groups),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "$groupLength members",
+                    style: const TextStyle(fontSize: 10),
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
         ),
       ],
     );
