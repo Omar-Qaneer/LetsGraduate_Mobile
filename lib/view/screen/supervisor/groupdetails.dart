8331dcda68090bae6d7faf809ecf27041d153f86
OMAR <s11925044@stu.najah.edu>
2023-12-27 12:17:19 +0200

    Continue GroupDetails Design
    Add ListView.Separated  to view students in this group dynamically

--- lib/view/screen/supervisor/groupdetails.dart
+++ lib/view/screen/supervisor/groupdetails.dart
@@ -16,18 +16,56 @@ class GroupDetails extends StatelessWidget {
         ),
         body: Container(
             padding: const EdgeInsets.symmetric(vertical: 30),
-            child: ListView(
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  // titles
-                  CustomTextSupervisor(
-                    text: "Click to see member details",
-                    fontSize: 25,
-                    horizontalPadding: 15,
-                    verticalPadding: 5,
-                  ),
+            child: ListView(children: [
+              // titles
+              const CustomTextSupervisor(
+                text: "Click to see member details",
+                fontSize: 25,
+                horizontalPadding: 15,
+                verticalPadding: 5,
+              ),
 
-                  CustomSearchBar(hintText: "Search by first or last name")
-                ])));
+              const CustomSearchBar(hintText: "Search by first or last name"),
+
+              ListView.separated(
+                shrinkWrap: true,
+                itemCount: group.length,
+                itemBuilder: (context, index) {
+                  final student = group.elementAt(index);
+                  return InkWell(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          radius: 25,
+                          backgroundColor: const Color(0xff764abc),
+                          child: Text(
+                              student['name'].toString().substring(0, 1),
+                              style: const TextStyle(
+                                  fontSize: 25, color: Colors.white))),
+                      title: Text(
+                        student['name'].toString(),
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      subtitle: Row(
+                        children: [
+                          const Icon(Icons.person),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            student['email'].toString(),
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                        ],
+                      ),
+                      trailing: const Icon(Icons.navigate_next),
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider();
+                },
+              )
+            ])));
   }
 }
