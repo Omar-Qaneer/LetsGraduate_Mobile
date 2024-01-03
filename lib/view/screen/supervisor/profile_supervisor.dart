d647d6f2103267b0fbc5141db3fcd9c33bccd586
OMAR <s11925044@stu.najah.edu>
2024-01-03 13:34:57 +0200

    Build Profile Page Part 1
     Add the Picture and Name of supervisor using CircleAvatar and Text Widgets

--- lib/view/screen/supervisor/profile_supervisor.dart
+++ lib/view/screen/supervisor/profile_supervisor.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/view/widget/supervisor/custommaterialbutton.dart';
+import 'package:flutter_application_1/view/widget/supervisor/customtextformfieldsupervisor.dart';
+import 'package:get/get.dart';
 
 class ProfileSupervisor extends StatelessWidget {
   const ProfileSupervisor({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Center(
-          child: Text("Profile Page"),
-        )
-      ],
+    Map supervisor = {
+      "name": "DR.Manar Qamhiee",
+      "email": "manarqamhiee@gmail.com",
+      "academicNumber": "1394",
+      "department": "Computer Engineering",
+      "address": "Nablus",
+      "mobileNumber": "0594251789",
+    };
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: ListView(
+        children: [
+          //profile picture
+          Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 24),
+                  child: const CircleAvatar(
+                    radius: 50,
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 100,
+                    ),
+                  ),
+                ),
+                Text(
+                  supervisor['name'],
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
